3e7b6aa14ca2cfaa631c53a3dc736d590ff49e36
Thịnh Phạm Sơn <pst269@gmail.com>
2020-03-16 20:49:51 +0700

    get_template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/home','HomeController@index');
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
